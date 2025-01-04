d6aa19e3d343f4254d84bb9442380f7c805d60e4
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-01-04 20:47:24 +0600

    count reset added

--- people counting/script.js
+++ people counting/script.js
@@ -1,5 +1,5 @@
 let countEl = document.getElementById("count-el")
-
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
@@ -7,5 +7,8 @@ function increment() {
     countEl.innerText = count
 }
 function save(){
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.innerText = 0
+    count = 0    
 }
